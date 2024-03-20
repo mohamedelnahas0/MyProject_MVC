f9d40e520709cfa4a6e08d0631c9ea6b96de1eb4
mohamedelnahas0 <muhmedelnhas@gmail.com>
2024-03-21 00:37:05 +0200

    Inteface And Repository for department Deprtment Migrations

---
+++ MyProject.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using MyProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department Get(int id);
+
+        int Add(Department entity);
+
+        int Update(Department entity);
+
+
+        int Delete(Department entity);
+
+
+
+
+
+    }
+}
---
+++ MyProject.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,63 @@
+﻿using MyProject.BLL.Interfaces;
+using MyProject.DAL.Data;
+using MyProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MyProject.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private  readonly ProjectDbContext _dbContext; //Null
+        public DepartmentRepository(ProjectDbContext DbContext) //ask clr to create object 
+        {
+            _dbContext = DbContext;
+        }
+
+        public int Add(Department entity)
+        {
+            _dbContext.departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(Department entity)
+        {
+            _dbContext.departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Delete(Department entity)
+        {
+            _dbContext.departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+          var departments = _dbContext.departments.Local.Where(D => D.Id == id).FirstOrDefault();
+
+
+            return _dbContext.Find<Department>(id); //EF  core 3.1 Feature
+
+
+            //return _dbContext.departments.Find(id);
+
+
+            //if (departments == null)
+            //{
+            //    departments = _dbContext.departments.Where(D => D.Id == id).FirstOrDefault();
+
+            //}
+            //return departments;
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbContext.departments.AsNoTracking().ToList();
+        }
+    }
+}
+ 
