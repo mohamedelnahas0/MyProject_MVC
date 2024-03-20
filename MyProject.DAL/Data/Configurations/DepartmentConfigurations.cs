d1fee4c26e14fac671ca214da16b5ceca0c74302
mohamedelnahas0 <muhmedelnhas@gmail.com>
2024-03-20 13:00:28 +0200

    DAL for Department MODULE

---
+++ MyProject.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using MyProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace MyProject.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //fluent Api for Department Domain
+
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);
+            builder.Property(D => D.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(D => D.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+
+
+
+        }
+    }
+}
---
+++ MyProject.DAL/Data/ProjectDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject.DAL.Data.Configurations;
+using MyProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.DAL.Data
+{
+    internal class ProjectDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        
+           => optionsBuilder.UseSqlServer("Server=.; Database=MVC_Project;trusted_connection=True;MultipleActiveResultSets=False");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> departments { get; set; }
+
+
+    }
+}
